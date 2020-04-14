f44884c415b91d7fe123a654df967e33dcf98d7a
VoltrefferNL <nielsdevisser@hotmail.com>
2020-04-14 12:47:25 +0100

    completed writing first test

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,12 +1,19 @@
-const { expect } = require('chai');
+const { expect } = require("chai");
 const {
   formatDates,
   makeRefObj,
   formatComments,
-} = require('../db/utils/utils');
+} = require("../db/utils/utils");
 
-describe('formatDates', () => {});
+describe("formatDates", () => {
+  it("Returns a new array when invoked", () => {
+    const unFormattedArticles = [];
+    const testFunc = formatDates(unFormattedArticles);
+    expect(testFunc).to.eql([]);
+    expect(testFunc).to.not.equal(unFormattedArticles);
+  });
+});
 
-describe('makeRefObj', () => {});
+describe("makeRefObj", () => {});
 
-describe('formatComments', () => {});
+describe("formatComments", () => {});
