cf30ad9c15d4fed38186e3051f5f812002dda349
VoltrefferNL <nielsdevisser@hotmail.com>
2020-04-14 13:24:13 +0100

    Added seedfile code untill articles seeding

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -2,17 +2,28 @@ const {
   topicData,
   articleData,
   commentData,
-  userData
-} = require('../data/index.js');
+  userData,
+} = require("../data/index.js");
 
-const { formatDates, formatComments, makeRefObj } = require('../utils/utils');
+const { formatDates, formatComments, makeRefObj } = require("../utils/utils");
 
-exports.seed = function(knex) {
-  const topicsInsertions = knex('topics').insert(topicData);
-  const usersInsertions = knex('users').insert(userData);
-
-  return Promise.all([topicsInsertions, usersInsertions])
+exports.seed = function (knex) {
+  return knex.migrate
+    .rollback()
+    .then(() => knex.migrate.latest())
     .then(() => {
+      const topicsInsertions = knex("topics").insert(topicData);
+      const usersInsertions = knex("users").insert(userData);
+      return Promise.all([topicsInsertions, usersInsertions]);
+    })
+    .then(() => {
+      const reformattedArticles = formatDates(articleData);
+      return knex("articles")
+        .insert(reformattedArticles)
+        .returning("*")
+        .then(() => {
+          console.log("Seeding untill Articles!");
+        });
       /* 
       
       Your article data is currently in the incorrect format and will violate your SQL schema. 
@@ -22,7 +33,7 @@ exports.seed = function(knex) {
       Your comment insertions will depend on information from the seeded articles, so make sure to return the data after it's been seeded.
       */
     })
-    .then(articleRows => {
+    .then((articleRows) => {
       /* 
 
       Your comment data is currently in the incorrect format and will violate your SQL schema. 
@@ -34,6 +45,6 @@ exports.seed = function(knex) {
 
       const articleRef = makeRefObj(articleRows);
       const formattedComments = formatComments(commentData, articleRef);
-      return knex('comments').insert(formattedComments);
+      return knex("comments").insert(formattedComments);
     });
 };
