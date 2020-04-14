1ac454707adf2bdcfe198f18edf0cb202e4a171c
VoltrefferNL <nielsdevisser@hotmail.com>
2020-04-14 11:28:20 +0100

    Created the first table (Topics) and added migrate-latest script

--- db/migrations/20200414110700_topics.js
+++ db/migrations/20200414110700_topics.js
@@ -1,8 +1,12 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  console.log("creating topics table in database");
+  return knex.schema.createTable("topics", (topicsTable) => {
+    topicsTable.string("slug").primary();
+    topicsTable.text("description");
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  console.log("Removing topics table from database");
+  return knex.schema.dropTable("houses");
 };
