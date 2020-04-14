4c5cc1e7e9169e6b48f50bea8bfee238fbf71540
VoltrefferNL <nielsdevisser@hotmail.com>
2020-04-14 11:38:36 +0100

    Created the second table (users) added additionals to topics

--- db/migrations/20200414110700_topics.js
+++ db/migrations/20200414110700_topics.js
@@ -1,12 +1,12 @@
 exports.up = function (knex) {
   console.log("creating topics table in database");
   return knex.schema.createTable("topics", (topicsTable) => {
-    topicsTable.string("slug").primary();
-    topicsTable.text("description");
+    topicsTable.string("slug").primary().unique();
+    topicsTable.text("description").notNullable();
   });
 };
 
 exports.down = function (knex) {
   console.log("Removing topics table from database");
-  return knex.schema.dropTable("houses");
+  return knex.schema.dropTable("topics");
 };
--- db/migrations/20200414110843_users.js
+++ db/migrations/20200414110843_users.js
@@ -1,8 +1,13 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  console.log("creating topics table in database");
+  return knex.schema.createTable("users", (usersTable) => {
+    usersTable.string("username").primary().unique();
+    usersTable.string("avatar_url").notNullable();
+    usersTable.string("name").notNullable();
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  console.log("Removing topics table from database");
+  return knex.schema.dropTable("users");
 };
