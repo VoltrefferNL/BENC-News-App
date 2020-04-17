97fbb185d720dc6f2a8074bb0dabbedb116ba39e
VoltrefferNL <nielsdevisser@hotmail.com>
2020-04-17 16:28:34 +0100

    Added 405 error to /api route

--- routes/api.router.js
+++ routes/api.router.js
@@ -3,16 +3,10 @@ const topicsRouter = require("./topics.router");
 const usersRouter = require("./users.router");
 const articlesRouter = require("./articles.router");
 const commentsRouter = require("./comments.router");
-const endpoints = require("../endpoints.json");
 const { send405Error } = require("../errors/");
+const { sendEndpoints } = require("../controllers/api.controllers");
 
-apiRouter.get(
-  "/",
-  (sendEndpoints = (req, res, next) => {
-    res.status(200).json(endpoints);
-  }),
-  send405Error
-);
+apiRouter.route("/").get(sendEndpoints).all(send405Error);
 
 apiRouter.use("/topics", topicsRouter);
 
