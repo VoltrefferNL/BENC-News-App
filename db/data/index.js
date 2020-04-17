9da013b19e61a4eec887219718513a1c52a571da
VoltrefferNL <nielsdevisser@hotmail.com>
2020-04-17 12:42:12 +0100

    changes to push to heroku

--- db/data/index.js
+++ db/data/index.js
@@ -3,6 +3,6 @@ const ENV = process.env.NODE_ENV || "development";
 const test = require("./test-data");
 const development = require("./development-data");
 
-const data = { development, test };
+const data = { development, test, production: development };
 
 module.exports = data[ENV];
