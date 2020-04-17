7dca5cbe8b60ac678544129505b6a2f6ce4a9271
VoltrefferNL <nielsdevisser@hotmail.com>
2020-04-17 12:53:20 +0100

    path error

--- listen.js
+++ listen.js
@@ -2,4 +2,3 @@ const { PORT = 9090 } = process.env;
 const app = require("./app.js");
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
-/home/eefflorrtv / Desktop / Northcoders / BEN / week - 3 / listen.js;
